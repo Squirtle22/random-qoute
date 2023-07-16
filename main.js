6c35ca024987253b13f5252c1788bbd18f3f85f4
Squirtle22 <djkeel@live.co.uk>
2023-07-16 15:43:42 +0100

    Random quote generator

---
+++ main.js
@@ -0,0 +1,38 @@
+// quotes in an array 
+
+const quotes = [
+    {
+        quote: 'Education is the most powerful weapon which you can use to change the world.',
+        source: 'Nelson Mandela',
+        year: 1990
+    },
+
+    {
+        quote: 'Be the change that you wish to see in the world.',
+        source: 'Mahatma Gandhi',
+        year: 1913
+    },
+
+    {
+        quote: 'If you want to know what a mans like, take a good look at how he treats his inferiors, not his equals.',
+        source: 'J.K. Rowling',
+        tag: 'Book, Harry potter and the Goblet of fire.',
+        year: 2000
+    },
+
+    {
+        quote: 'Never let the fear of striking out keep you from playing the game',
+        source: 'Babe Ruth',
+        tag: 'Baseball player',
+    },
+
+    {
+        quote: 'Life imposes things on you that you can`t control, but you still have the choice of how you`re going to live through this.',
+        source: 'Celine Dion',
+        tag: 'Canadian Singer',
+    }
+]
+
+function getRandomQuote() {
+    let RandomQuote = 
+}
